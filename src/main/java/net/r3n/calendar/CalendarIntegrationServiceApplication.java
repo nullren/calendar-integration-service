56e18d812a547742908d3eff3a1ff4f6c2bd8eca
Renning Bruns <renning.bruns@gmail.com>
2019-06-02 11:24:11 -0700

    spring initializr

---
+++ src/main/java/net/r3n/calendar/CalendarIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package net.r3n.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarIntegrationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CalendarIntegrationServiceApplication.class, args);
+  }
+
+}
